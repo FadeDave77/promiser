ae65ba8c1f88f68ca0f34521d329d1985fb94d3a
FadeDave77 <64272263+FadeDave77@users.noreply.github.com>
2021-03-16 00:49:39 +0100

    rejection actually returns the error, and splits it into two if >2000 chars

--- src/listeners/client/unhandledrejection-listener.ts
+++ src/listeners/client/unhandledrejection-listener.ts
@@ -11,6 +11,13 @@ export default class UnhandledRejectionListener extends Listener {
 	public async exec(error: string): Promise<void> {
 		console.error(error);
 		(await this.client.users.fetch(OwnerId)).dmChannel!.send(` I have encountered an error on ${this.client.user!.tag} @ ${new Date().toString().substr(0, 31)} o.O`);
-		(await this.client.users.fetch(OwnerId)).dmChannel!.send('`error`');
+		if (error.length >= 2000) {
+			const err1 = error.slice(0, error.length / 2);
+			const err2 = error.slice(error.length / 2, error.length);
+			const channel = await (await this.client.users.fetch(OwnerId)).dmChannel;
+			channel?.send(err1);
+			channel?.send(err2);
+		}
+		else { (await this.client.users.fetch(OwnerId)).dmChannel!.send('`' + error + '`'); }
 	}
 }
