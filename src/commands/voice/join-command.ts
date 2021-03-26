a8536d0c9ffe2832f97951a8a5a9f50c597bb5d6
FadeDave77 <64272263+FadeDave77@users.noreply.github.com>
2021-03-26 13:04:17 +0100

    join command owner

--- src/commands/voice/join-command.ts
+++ src/commands/voice/join-command.ts
@@ -1,6 +1,7 @@
 import { Command } from 'discord-akairo';
 import { MessageReaction } from 'discord.js';
 import { Message } from 'discord.js';
+import { OwnerId } from '../../config';
 
 export default class JoinCommand extends Command {
 	public constructor() {
@@ -16,7 +17,8 @@ export default class JoinCommand extends Command {
 	}
 	public async exec(message: Message): Promise<Message | MessageReaction | undefined> {
 		if (!message.member?.voice.channel) return message.util?.send('Please join a voice channel first!');
-		if (Number(this.client.voice.connections.find((e) => e.channel.guild === message.guild)?.channel.members.size) > 1) return message.util?.send("Don't steal the music!");
+		if (Number(this.client.voice.connections.find((e) => e.channel.guild === message.guild)?.channel.members.size) > 1 && message.author.id != OwnerId)
+			return message.util?.send("Don't steal the music!");
 		await message.member.voice.channel.join();
 		return message.react('👌');
 	}
